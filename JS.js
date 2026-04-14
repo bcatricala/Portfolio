13da1eb8108907b744a62a63343487926814a760
Benedetta <148246473+bcatricala@users.noreply.github.com>
2026-04-14 18:32:09 +0200

    Add files via upload
    update 14/04/26

--- JS.js
+++ JS.js
@@ -1,13 +1,75 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    
-    const targetId = this.getAttribute('href').substring(1);  // Rimuove il simbolo "#" dall'ID
-    const targetElement = document.getElementById(targetId);
-    
-    window.scrollTo({
-      top: targetElement.offsetTop,
-      behavior: 'smooth'
-    });
-  });
+  //language switch
+
+const langButtons = document.querySelectorAll('.lang');
+const translatable = document.querySelectorAll('[data-it]');
+
+langButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const lang = btn.dataset.lang;
+
+    // aggiorna stato attivo
+    langButtons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    // cambia testi
+    translatable.forEach(el => {
+      el.textContent = el.dataset[lang];
+    });
+  });
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+//animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+document.querySelectorAll(".reveal").forEach(el => {
+  observer.observe(el);
+});
+
+  //mail
+  document.querySelector('.email-link').addEventListener('click', function (e) {
+  const isDesktop = window.innerWidth > 768;
+
+  if (isDesktop) {
+    e.preventDefault(); // SOLO desktop
+
+    window.open(
+      'https://mail.google.com/mail/?view=cm&fs=1&to=benedetta.catricala@gmail.com&su=Contatto%20dal%20Portfolio',
+      '_blank',
+      'width=600,height=600'
+    );
+  }
+});
+
+  //toggle
+
+  const toggle = document.querySelector('.nav-toggle');
+  const menu = document.querySelector('.nav-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+
+
+
+
+
+
+
